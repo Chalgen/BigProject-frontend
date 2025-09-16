41e6456c44fa0b502d848501d1720e68eff0d6c0
chtholly-daisuki <lhr_0214@163.com>
2025-09-16 14:27:58 +0800

    test2

---
+++ 学生服务平台test/src/views/index.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template scoped>
+    
+    <div class="oricon">
+        欢迎使用学生服务平台
+        <br></br>
+        制作人：
+        <br></br>
+        <div>
+          <router-link :to="{path:'login'}" class="btn">开始登陆</router-link>
+        </div>
+        
+    </div>
+</template>
+
+<style scoped>
+.oricon{
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  max-width: 800px;
+  margin: 2rem auto;
+  padding: 0 1rem;
+}
+.btn {
+  display:flex;
+  padding: 8px 16px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  /*cursor: pointer;*/
+  text-decoration: none; /* 去除链接默认下划线 */
+}
+</style>
