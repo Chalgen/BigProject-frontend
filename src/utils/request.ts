0dfb17485593cb17723d64be41677975979c9f33
chtholly-daisuki <lhr_0214@163.com>
2025-10-04 04:40:53 +0800

    对接成功、小功能实现

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -7,10 +7,6 @@ const request = axios.create({
   //baseURL: "http://127.0.0.1:4523/m1/7131475-6854516-default"
   //baseURL: import.meta.env.VITE_API_BASE_URL,
   //timeout: 5000
-  //baseURL: "http://127.0.0.1:8080"
-  //baseURL: "http://127.0.0.1:4523/m1/7120556-6843396-default"
-  //baseURL: "http://192.168.43.155:8080"
-  baseURL: "http://192.168.137.191:8080"
 })
 
 export default request
